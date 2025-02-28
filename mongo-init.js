7c34ce5105e2a9330fd203e7272dc7afad7a10f6
Nisarg Panchal <nisarg.panchal@justice.qld.gov.au>
2025-02-28 15:17:17 +1000

    mongo-express changes

--- mongo-init.js
+++ mongo-init.js
@@ -3,5 +3,9 @@ db = db.getSiblingDB("admin");
 db.createUser({
     user: "user1",
     pwd: "user1",
-    roles: [{ role: "readWrite", db: "people-db" }]
+    roles: [
+        { role: "root", db: "admin" },                 // Full admin access
+        { role: "readWrite", db: "people-db" },        // Read/write access to your app DB
+        { role: "readAnyDatabase", db: "admin" },      // Allow reading any database
+        { role: "clusterMonitor", db: "admin" }     ]
 });
